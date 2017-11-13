62be4ab0e2bf1227178ed2c66a63edb0014535da
Semi Turdean <Semiturdean@hotmail.com>
2017-11-13 12:51:09 +0100

    testar igen

--- Grupparbete/src/quizkampen/Frame.java
+++ Grupparbete/src/quizkampen/Frame.java
@@ -19,6 +19,7 @@ public class Frame extends JFrame implements ActionListener {
 	JButton newGame = new JButton("Starta ett nytt spel");
 	
 	public Frame() {
+		//frame layout etc
 		setLayout(new BorderLayout());
 		add("North", userInfo);
 		
