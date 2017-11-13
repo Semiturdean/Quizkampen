eb4c76a9dbb14ea66f693ed6c35b5895fe2dcb67
sinasa2 <sinasa2@yahoo.com>
2017-11-13 12:42:26 +0100

    Player class lagt till

---
+++ Grupparbete/src/quizkampen/Player.java
@@ -0,0 +1,41 @@
+package quizkampen;
+
+public class Player 
+{
+	private String username;
+	private String password;
+	private String membershipType;
+	
+	public Player(String username)
+	{
+		this.username = username;
+		membershipType = "basic";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMembershipType() {
+		return membershipType;
+	}
+
+	public void setMembershipType(String membershipType) {
+		this.membershipType = ((membershipType == "basic") || (membershipType == "premium") ? membershipType : null);
+	}
+	
+	
+
+}
