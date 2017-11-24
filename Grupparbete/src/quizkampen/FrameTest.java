e0282d0febeb9d95d624bebe35bf9ebf8ac02980
Semi Turdean <Semiturdean@hotmail.com>
2017-11-24 11:36:49 +0100

    removed unused imports

--- Grupparbete/src/quizkampen/FrameTest.java
+++ Grupparbete/src/quizkampen/FrameTest.java
@@ -5,14 +5,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 
