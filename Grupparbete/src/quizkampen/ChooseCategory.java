590a804802a536b71b65d792650567605006b7a5
Senad Hasic <senad.hasic@gmail.com>
2017-11-24 11:40:22 +0100

    first

--- Grupparbete/src/quizkampen/ChooseCategory.java
+++ Grupparbete/src/quizkampen/ChooseCategory.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class ChooseCategory extends SearchCategory {
 
     private File Music = new File("C:\\Users\\Senad Hasic\\IdeaProjects\\Quizkampen\\Grupparbete\\src\\Musik.txt");
-    private File Geografi = new File("src\\Geografi.txt");
+    private File Geografi = new File("C:\\Users\\Senad Hasic\\IdeaProjects\\Quizkampen\\Grupparbete\\src\\Geografi.txt");
 
     ChooseCategory(String category) throws IOException {
         if (category.equalsIgnoreCase("Musik")) {
