1dff3c1a7c8680bced3e459181de60177238e32a
Semi Turdean <Semiturdean@hotmail.com>
2017-11-29 12:02:49 +0100

    fixed the graphics a bit

--- Grupparbete/src/quizkampen/Client.java
+++ Grupparbete/src/quizkampen/Client.java
@@ -1,14 +1,18 @@
 package quizkampen;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,7 +24,15 @@ public class Client extends JFrame implements ActionListener {
     private JTextField textField = new JTextField(10);
     private JButton categoryButton = new JButton("Skicka kategori");
     private JButton sendAnswer = new JButton("Send answer");
+    private JPanel componentPanel = new JPanel();
+    private JPanel picturePanel = new JPanel();
+    private JLabel picLabel = new JLabel();
     private String text = "";
+    private ImageIcon icon = new ImageIcon();
+    private JButton newGame = new JButton("Starta ett nytt spel");
+	private final JLabel userLabel = new JLabel("Ange ett användarnamn:");
+	private JLabel userName = new JLabel("");
+	private JTextField userNameInput = new JTextField(10);
 
     Client(String serverAddress, int port) {
         try {
@@ -28,17 +40,40 @@ public class Client extends JFrame implements ActionListener {
             input = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
             output = new PrintWriter(clientConnection.getOutputStream(), true);
             continueGame = true;
-
+            URL url = new URL
+            ("https://res-2.cloudinary.com/westfielddg/image/fetch/c_pad,f_auto,q_auto/http://res.cloudinary.com/westfielddg/image/upload/fph0rlhpygetnd63ydxz.png");
+            BufferedImage img = ImageIO.read(url);
+            icon.setImage(img);
+            
+            
             setLayout(new BorderLayout());
             setBackground(Color.WHITE);
-            add(textField);
-            add(categoryButton);
-            add(sendAnswer);
+            add("North", componentPanel);
+            componentPanel.add(userLabel);
+            componentPanel.add(userName);
+            componentPanel.add(userNameInput);
+            componentPanel.add(textField);
+            componentPanel.add(newGame);
+            componentPanel.add(categoryButton);
+            componentPanel.add(sendAnswer);
+            componentPanel.setBackground(Color.WHITE);
+            
+            categoryButton.setVisible(false);
+            sendAnswer.setVisible(false);
+            newGame.setVisible(false);
+            textField.setVisible(false);
+            
             categoryButton.addActionListener(this);
             sendAnswer.addActionListener(this);
             textField.addActionListener(this);
-
-            setSize(300,300);
+            userNameInput.addActionListener(this);
+            newGame.addActionListener(this);
+            add("Center",picturePanel);
+            picLabel.setIcon(icon);
+            picturePanel.add(picLabel);
+            picturePanel.setBackground(Color.WHITE);
+            
+            setSize(600,600);
             setLocationRelativeTo(null);
             setDefaultCloseOperation(3);
             setVisible(true);
@@ -46,11 +81,21 @@ public class Client extends JFrame implements ActionListener {
             //frame = new Frame();
 
             startGame();
+        }catch (MalformedURLException e) {
+			e.printStackTrace();
         } catch (IOException e) {
             System.out.println("Could not connect to server");
         }
     }
-
+    
+    private void startToGamePanel(){
+    	componentPanel.remove(newGame);
+    	textField.setVisible(true);
+    	categoryButton.setVisible(true);
+    	sendAnswer.setVisible(true);
+    	
+    }
+    
     private void sendCategory(String category) {
         output.println(Commands.CATEGORY + category);
     }
@@ -125,8 +170,18 @@ public class Client extends JFrame implements ActionListener {
             sendCategory(text);
             text = "";
         }
-        if(e.getSource() == textField){
-
+        if(e.getSource() == userNameInput) {
+        	if(!userNameInput.getText().equals("")) {
+        		userName.setText(userNameInput.getText());
+        		componentPanel.remove(userNameInput);
+        		newGame.setVisible(true);
+        		userLabel.setText("Användare:");
+        	}
+        }
+        
+        if(e.getSource() == newGame){
+        	startToGamePanel();
+        	
         }
         if (e.getSource() == sendAnswer) {
             text = Commands.ANSWER.toString();
